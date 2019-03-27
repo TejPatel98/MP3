d36e743b3219047b2f4513c95bb219c628cf36e8
Tej Patel <tpa244@uky.edu>
2019-03-27 13:38:32 -0400

    q4

--- mp3/jenkins/test/src/test/java/jenkins/model/newTests.java
+++ mp3/jenkins/test/src/test/java/jenkins/model/newTests.java
@@ -1,4 +1,3 @@
-/*
 package jenkins.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -51,73 +50,7 @@ import org.mockito.Mockito;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
-
-
-
-*/
-
-
-
-
-package jenkins.model;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
-
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.HttpMethod;
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.WebRequestSettings;
-import com.gargoylesoftware.htmlunit.WebResponse;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
-import hudson.maven.MavenModuleSet;
-import hudson.maven.MavenModuleSetBuild;
-import hudson.model.Failure;
-import hudson.model.RestartListener;
-import hudson.model.RootAction;
-import hudson.model.UnprotectedRootAction;
-import hudson.model.User;
-import hudson.security.FullControlOnceLoggedInAuthorizationStrategy;
-import hudson.security.HudsonPrivateSecurityRealm;
-import hudson.util.HttpResponses;
 import jenkins.model.IdStrategy.CaseSensitive;
-import hudson.model.FreeStyleProject;
-import hudson.security.GlobalMatrixAuthorizationStrategy;
-import hudson.security.LegacySecurityRealm;
-import hudson.security.Permission;
-import hudson.slaves.ComputerListener;
-import hudson.slaves.DumbSlave;
-import hudson.slaves.OfflineCause;
-import hudson.util.FormValidation;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.jvnet.hudson.test.Issue;
-import org.jvnet.hudson.test.ExtractResourceSCM;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.JenkinsRule.WebClient;
-import org.jvnet.hudson.test.TestExtension;
-import org.kohsuke.stapler.HttpResponse;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-
-
-
-
-
 
 
 public class newTests{
